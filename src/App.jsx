6ffaaf4bb9fb72aeaefca25fb7143d4819fc07ab
mmoussa720 <mohamed.moussa.0720@gmail.com>
2026-03-18 17:09:50 +0100

    completed the about section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import gsap from "gsap";
 import NavBar from "./components/NavBar.jsx";
 import Hero from "./components/Hero.tsx";
 import Cocktails from "./components/Cocktails.jsx";
+import About from "./components/About.jsx";
 gsap.registerPlugin(ScrollTrigger,SplitText);
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
           <NavBar />
           <Hero />
           <Cocktails/>
+          <About />
       </main>
   );
 };
