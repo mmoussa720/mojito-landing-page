5726f4f039df8481c6fb1493624c241079059af3
mmoussa720 <mohamed.moussa.0720@gmail.com>
2026-03-18 02:34:21 +0100

    completed the hero section by adding a video moves frame by frame when scrolling

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,84 @@
-import React from 'react';
-
+import React, {useRef} from 'react';
+import {useGSAP} from "@gsap/react";
+import {SplitText} from "gsap/all"
+import gsap from "gsap";
+import {useMediaQuery} from "react-responsive";
 const Hero = () => {
+  const videoRef = useRef(null);
+  const isMobile=useMediaQuery({maxWidth:767});
+  useGSAP(()=>{
+    const heroSplit=new SplitText(
+        '.title',{type:'chars, words'}
+    );
+    const paragraphSplit=new SplitText('.subtitle',{type:'lines'});
+    heroSplit.chars.forEach((char)=>char.classList.add('text-gradient'));
+    gsap.from(heroSplit.chars,{
+      yPercent:100,
+      duration:1.8,
+      ease:'expo.out',
+      stagger:0.06,
+    });
+    gsap.from(paragraphSplit.lines,{
+      opacity:0,
+      yPercent:100,
+      duration:1.8,
+      ease:'expo.out',
+      stagger:0.06,
+      delay:1,
+    });
+    gsap.timeline(
+        {
+          scrollTrigger:{
+            trigger:'#hero',
+            start:'top top,',
+            end:'bottom top',
+            scrub:true,
+          }
+        }
+    ).to('.right-leaf',{y:200},0).to('.left-leaf',{y:-200},0);
+    const startValue=isMobile?'top 50%':'center 60%';
+    const endValue=isMobile?'120% top':'bottom top';
+    const tl=gsap.timeline({
+      scrollTrigger:{
+        trigger:'video',
+        start:startValue,
+        end:endValue,
+        scrub:true,
+        pin:true,
+      }
+    });
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      });
+    };
+  },[]);
   return (
-      <section id="hero" className="noisy">
-        <h1 className="title">MOJITO</h1>
-        <img src="/images/hero-left-leaf.png" alt={"left-leaf"}
-             className=
-      </section>
+<>
+  <section id="hero" className="noisy">
+    <h1 className="title">MOJITO</h1>
+    <img src="/images/hero-left-leaf.png" alt={"left-leaf"}
+         className="left-leaf"/>
+    <img src="/images/hero-right-leaf.png" alt={"left-leaf"}
+         className="right-leaf"/>
+    <div className="body">
+      <div className="content">
+        <div className="space-y-5 hidden md:block">
+          <p>Cool. Crisp. Classic.</p>
+          <p className="subtitle">Sip the Spirit <br/> of Summer</p>
+        </div>
+        <div className="view-cocktails">
+          <p className="subtitle">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.
+          </p>
+          <a href="#cocktails">View Cocktails</a>
+        </div>
+      </div>
+    </div>
+  </section>
+  <div className="video absolute inset-0">
+    <video ref={videoRef}  src="/videos/output.mp4" muted playsInline preload="auto" />
+  </div></>
   );
 };
-
 export default Hero;
